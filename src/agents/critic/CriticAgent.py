a3f9d8a33b150f4d1e5b4dd0e37cc6b9ff66dde9
joao-pedro-angelo <carneiroangelojoaopedro@gmail.com>
2024-12-15 12:30:19 -0300

    comment

--- src/agents/critic/CriticAgent.py
+++ src/agents/critic/CriticAgent.py
@@ -2,7 +2,7 @@ from autogen import ConversableAgent
 from openai import OpenAI
 import os
 
-# Carrega a chave da API e # verifica se o carregamento deu certo
+# Carrega a chave da API e verifica se o carregamento deu certo
 api_key = os.getenv("OPENAI_API_KEY").strip()  # Remove qualquer caractere extra
 if not api_key:
     print("Erro: chave da API não encontrada.")
