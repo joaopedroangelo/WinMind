0c376452f6402a07eceef56b8bf03603f5d26cb8
Lucas Raniere <lucas.raniere94@gmail.com>
2024-12-19 16:01:36 -0300

    removed unused code

--- src/agents/CaptureDataAgent.py
+++ src/agents/CaptureDataAgent.py
@@ -21,13 +21,5 @@ class DataCaptureAgent:
         )
 
 
-    def get_system_message(self):
-        return (
-            'Você é um agente de captura de dados em um sistema projetado para ajudar os usuários'
-            'a gerenciar o vício em jogos de azar em apostas esportivas. Sua função é receber o perfil '
-            'do usuário contendo dados e repassar para o agente de gerenciamento de chat.'
-        )
-
-
     def get_agent(self):
         return self.agent
